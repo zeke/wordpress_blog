e548d0d2873d7d3916a63c66d6a1951e0539f12d
Zeke Sikelianos <zeke@sikelianos.com>
2008-11-22 15:00:04 -0800

    More styling

--- wp-content/themes/zeke/header.php
+++ wp-content/themes/zeke/header.php
@@ -15,11 +15,6 @@
 <body>
 <div id="page">
 
-
 <div id="header">
-	<div id="headerimg">
-		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<div class="description"><?php bloginfo('description'); ?></div>
-	</div>
 </div>
 <hr />
