e1b9bf0e0311b573b815f5208b0485d89db9abbf
Zeke Sikelianos <zeke@sikelianos.com>
2009-01-04 12:13:13 -0800

    Working on single page style

--- wp-content/themes/zeke/single.php
+++ wp-content/themes/zeke/single.php
@@ -4,11 +4,6 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<div class="navigation">
-			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
-			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
-		</div>
-
 		<div class="post" id="post-<?php the_ID(); ?>">
 			<h2><?php the_title(); ?></h2>
 
@@ -49,6 +44,11 @@
 
 					</small>
 				</p>
+				
+    		<div class="navigation">
+    			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
+    			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
+    		</div>
 
 			</div>
 		</div>
