71cd84a81065ca75f412412424655b9818a7380b
Zeke Sikelianos <zeke@sikelianos.com>
2009-01-04 16:13:43 -0800

    styling footer

--- wp-content/themes/zeke/footer.php
+++ wp-content/themes/zeke/footer.php
@@ -1,10 +1,9 @@
-
 <hr />
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
 	<p>
-		The <?php bloginfo('name'); ?> Blohg is powered by <a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		Zeke Sikelianos' Blohg is powered by <a href="http://wordpress.org/">WordPress</a>. 
+    Got a feed reader? Subscribe to the <a href="<?php bloginfo('rss2_url'); ?>">RSS Feed</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
